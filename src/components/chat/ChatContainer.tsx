5e8d6932d589a30ad28d22c43cdc69e749873c62
harby0757 <harby.ahn@peernine.co.kr>
2025-01-26 13:50:50 +0900

    refactor: 디버그 모드 기본값을 false로 변경하고 ChatContainer의 로그 출력을 디버그 모드에 따라 조건부로 수정

--- src/components/chat/ChatContainer.tsx
+++ src/components/chat/ChatContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useMemo, useCallback, useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import { debugModeState } from '@/store/debug';
 import { useChat } from '@/hooks/useChat';
@@ -6,13 +6,13 @@ import { ChatWrapper, ContentSection, MessageSection, InputSection } from './Cha
 import { Splash } from '@/components/common/Splash';
 import ChatBubble from './ChatBubble';
 import Slider from './Slider';
-import { TextMessage, SliderMessage } from '@/types/chat';
+import { TextMessage, SliderMessage, ChatMessage, ChatType } from '@/types/chat';
 
 interface Props {
   $inputEnabled?: boolean;
 }
 
-const ChatContainer: React.FC<Props> = ({ $inputEnabled = true }) => {
+const ChatContainer: React.FC<Props> = React.memo(({ $inputEnabled = true }) => {
   const isDebugMode = useRecoilValue(debugModeState);
   const lastBubbleRef = useRef<HTMLDivElement>(null);
   const {
@@ -25,59 +25,86 @@ const ChatContainer: React.FC<Props> = ({ $inputEnabled = true }) => {
     setLoadingStep
   } = useChat({ isDebugMode });
 
+  // 메시지 상태 모니터링
+  useEffect(() => {
+    if (isDebugMode) {
+      console.log('Current messages in ChatContainer:', messages);
+    }
+  }, [messages, isDebugMode]);
+
+  const handleLoadingComplete = useCallback(() => {
+    setLoadingStep(0);
+  }, [setLoadingStep]);
+
+  const renderChatBubble = useCallback((message: TextMessage, index: number, prevType?: ChatType, prevHasLink?: boolean) => (
+    <div key={message.id} ref={index === messages.length - 1 ? lastBubbleRef : undefined}>
+      <ChatBubble
+        message={message}
+        prevType={prevType}
+        prevHasLink={prevHasLink}
+        isWaiting={message.id === waitingMessageId}
+      />
+    </div>
+  ), [messages.length, waitingMessageId]);
+
+  const renderSlider = useCallback((message: SliderMessage) => (
+    <Slider
+      key={message.id}
+      items={message.sliderData}
+      onComplete={() => {
+        if (isDebugMode) {
+          console.log('Slider complete');
+        }
+      }}
+    />
+  ), [isDebugMode]);
+
+  const renderMessage = useCallback((message: ChatMessage, index: number) => {
+    const prevMessage = index > 0 ? messages[index - 1] : null;
+    const prevType = prevMessage?.type;
+    const prevHasLink = prevMessage && 'link' in prevMessage ? !!prevMessage.link : false;
+
+    switch (message.type) {
+      case 'slider':
+        return renderSlider(message as SliderMessage);
+      case 'jerry':
+      case 'user':
+        return renderChatBubble(message as TextMessage, index, prevType, prevHasLink);
+      default:
+        return null;
+    }
+  }, [messages, renderChatBubble, renderSlider]);
+
+  const loadingOverlay = useMemo(() => {
+    if (!showLoading) return null;
+    
+    return (
+      <Splash
+        variant="flowItem"
+        variantProps={{ 
+          $prevElement: lastBubbleRef.current 
+        }}
+        message={loadingMessages[loadingStep] || ''}
+        isVisible={loadingStep !== loadingMessages.length}
+        animation="fade"
+        onComplete={handleLoadingComplete}
+      />
+    );
+  }, [showLoading, loadingStep, loadingMessages, handleLoadingComplete]);
+
+  const messageList = useMemo(() => messages.map(renderMessage), [messages, renderMessage]);
+
   return (
     <ChatWrapper data-debug={isDebugMode}>
       <ContentSection>
         <MessageSection data-debug={isDebugMode}>
-          {messages.map((message, index) => {
-            const prevMessage = index > 0 ? messages[index - 1] : null;
-            const prevType = prevMessage?.type;
-            const prevHasLink = prevMessage && 'link' in prevMessage ? !!prevMessage.link : false;
-
-            if (message.type === 'slider') {
-              return (
-                <Slider
-                  key={message.id}
-                  items={(message as SliderMessage).sliderData}
-                  onComplete={() => {}}
-                />
-              );
-            }
-
-            if (message.type === 'jerry' || message.type === 'user') {
-              return (
-                <div key={message.id} ref={index === messages.length - 1 ? lastBubbleRef : undefined}>
-                  <ChatBubble
-                    message={message}
-                    prevType={prevType}
-                    prevHasLink={prevHasLink}
-                    isWaiting={message.id === waitingMessageId}
-                  />
-                </div>
-              );
-            }
-
-            return null;
-          })}
-          {showLoading && (
-            <Splash
-              variant="flowItem"
-              variantProps={{ 
-                prevElement: lastBubbleRef.current 
-              }}
-              message={loadingMessages[loadingStep] || ''}
-              isVisible={loadingStep !== loadingMessages.length}
-              animation="fade"
-              onComplete={() => {
-                setLoadingStep(0);
-              }}
-            />
-          )}
+          {messageList}
+          {loadingOverlay}
         </MessageSection>
         <InputSection data-debug={isDebugMode} $inputEnabled={$inputEnabled} />
       </ContentSection>
     </ChatWrapper>
   );
-};
+}, (prevProps, nextProps) => prevProps.$inputEnabled === nextProps.$inputEnabled);
 
 export default ChatContainer; 
--- src/store/debug.ts
+++ src/store/debug.ts
@@ -2,5 +2,5 @@ import { atom } from 'recoil';
 
 export const debugModeState = atom<boolean>({
   key: 'debugModeState',
-  default: true,
+  default: false,
 }); 
